dd4b2ef0ab2faf5685a839a5cceb855cdedd4443
Joallyfer <joallyfer@gmail.com>
2025-11-24 21:25:59 -0300

    realizada a migracao

---
+++ API/API/Migrations/20251125002225_Inicial.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace API.Migrations
+{
+    /// <inheritdoc />
+    public partial class Inicial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Chamados",
+                columns: table => new
+                {
+                    ChamadoId = table.Column<string>(type: "TEXT", nullable: false),
+                    Descricao = table.Column<string>(type: "TEXT", nullable: true),
+                    CriadoEm = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    Status = table.Column<string>(type: "TEXT", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Chamados", x => x.ChamadoId);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Chamados",
+                columns: new[] { "ChamadoId", "CriadoEm", "Descricao", "Status" },
+                values: new object[,]
+                {
+                    { "2f1b7dc1-3b9a-4e1a-a389-7f5d2f1c8f3e", new DateTime(2025, 11, 27, 21, 22, 24, 553, DateTimeKind.Local).AddTicks(3172), "Trocar tinta da impressora", "Aberto" },
+                    { "6a8b3e4d-5e4e-4f7e-bdc9-9181e456ad0e", new DateTime(2025, 12, 1, 21, 22, 24, 553, DateTimeKind.Local).AddTicks(3155), "Formatar computador", "Aberto" },
+                    { "e5d4a7b9-1f9e-4c4a-ae3b-5b7c1a9d2e3f", new DateTime(2025, 12, 8, 21, 22, 24, 553, DateTimeKind.Local).AddTicks(3177), "Trocar teclado", "Aberto" },
+                    { "e5d4a7b9-1f9e-4c4a-ae3b-5b7c1a9d2e3h", new DateTime(2025, 11, 26, 21, 22, 24, 553, DateTimeKind.Local).AddTicks(3183), "Trocar o Mouse", "Em andamento" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Chamados");
+        }
+    }
+}
