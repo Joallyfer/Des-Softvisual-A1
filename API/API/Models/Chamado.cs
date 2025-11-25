612990d066da45468a63ed5f2a2bb814a950be64
Joallyfer <joallyfer@gmail.com>
2025-11-24 21:18:58 -0300

    Alteração na classe chamado

--- API/API/Models/Chamado.cs
+++ API/API/Models/Chamado.cs
@@ -5,5 +5,5 @@ public class Chamado
     public string ChamadoId { get; set; } = Guid.NewGuid().ToString();
     public string? Descricao { get; set; }
     public DateTime CriadoEm { get; set; } = DateTime.Now;
-    public string? Status { get; set; } = "Aberto";
+    public string? Status { get; set; } 
 }
